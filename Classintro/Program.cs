1c9c6eab4acbb2211a893d27498b8fcb9ef57a0a
SyS <SyS@DESKTOP-4QOI5H8>
2023-01-30 18:27:34 +0300

    Add project files.

---
+++ Classintro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime;
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Berkay Demiroğ";
+            kurs2.IzlenmeOrani = 80;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Altay Demiroğ";
+            kurs3.IzlenmeOrani = 90;
+
+        Console.WriteLine(kurs1.KursAdi + ":" + kurs1.Egitmen);
+
+        Console.WriteLine("Hello World");
+        }
+    }
+    class Kurs
+    {
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int IzlenmeOrani { get; set; }
+
+
+    }
+
+}
